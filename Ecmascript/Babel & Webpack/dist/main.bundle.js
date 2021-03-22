3fe6d89ad59f8c3681c7c224170f7a27da401cec
joel godenir <contact@joel-godenir.fr>
2021-03-22 14:19:43 +0100

    commit

---
+++ Ecmascript/Babel & Webpack/dist/main.bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var maVar = "hoho";
+alert(maVar);
+/******/ })()
+;
+//# sourceMappingURL=main.bundle.js.map
